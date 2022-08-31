75ce36a405f2578777ed394ec7445a2464e01a0d
Matrix <1337816495@qq.com>
2022-09-01 01:23:01 +0800

    Update App.js

--- source/App.js
+++ source/App.js
@@ -1,32 +1,5 @@
 import React from "react";
-import { useState, createContext, useContext } from "react";
 
-const CountContext = createContext();
-
-function CountProvider ( property ) {
-
-    const value = useState( 0 );
-
-    return <CountContext.Provider value={ value } { ... property }/>;
-
-}
-
-function Counter () {
-
-    const [ count, setCount ] = useContext( CountContext );
-
-    return <button onClick={ _ => setCount( count + 1 ) }>{ count }</button>;
-
-}
-
-function App () {
-
-    return (
-        <CountProvider>
-            <Counter/>
-        </CountProvider>
-    );
-
-}
+function App () {}
 
 export default App;
